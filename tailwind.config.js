f6727d164f88d3adcf2208139e410a697264843a
Dennis Campos <dennisjr89@gmail.com>
2022-05-22 18:55:57 -0400

    fix: slight configuration to css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-    backgroundColor: {
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      tertiary: 'var(--color-tertiary)',
-      quaternary: 'var(--color-quaternary)',
-      quinary: 'var(--color-quinary)',
-    },
-    fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-family)',
+      },
+      backgroundColor: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        quaternary: 'var(--color-quaternary)',
+        quinary: 'var(--color-quinary)',
+      },
     },
   },
   plugins: [],
