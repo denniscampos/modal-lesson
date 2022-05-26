3171ad5054032db04968062f24818bd3237393e8
Dennis Campos <dennisjr89@gmail.com>
2022-05-25 21:47:03 -0400

    fix: hide sidebar when on login route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,18 @@ import { client } from '@/lib/apolloClient';
 import { SessionProvider } from 'next-auth/react';
 import { MainLayout } from '@/layout/MainLayout';
 import { Sidebar } from '@/components/Sidebar';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
+  const loginPage = router.pathname === '/login' ? false : true;
+
   return (
     <SessionProvider session={session}>
       <ApolloProvider client={client}>
         <MainLayout>
-          <Sidebar />
+          {loginPage && <Sidebar />}
           <Component {...pageProps} />
         </MainLayout>
       </ApolloProvider>
