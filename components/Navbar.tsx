fddaff97ae33abd54a7fda4e3bc1fecc608aa292
Dennis Campos <dennisjr89@gmail.com>
2022-05-27 23:18:33 -0400

    add function for navbar to fix repeats

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,8 +39,12 @@ export default function Navbar() {
                   />
                 </div>
                 <div className="hidden md:ml-6 md:flex md:space-x-8">
-                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                  <a
+                  <DisclosureButton activeText="Dashboard" inActiveText="hey" />
+                  {/* <DisclosureButton inActiveText="Team" /> */}
+                </div>
+                {/* <div className="hidden md:ml-6 md:flex md:space-x-8"> */}
+                {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                {/* <a
                     href="#"
                     className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
@@ -64,7 +68,7 @@ export default function Navbar() {
                   >
                     Calendar
                   </a>
-                </div>
+                </div> */}
               </div>
               <div className="flex items-center">
                 <div className="flex-shrink-0">
@@ -235,5 +239,25 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
+  );
+}
+
+function DisclosureButton({ ...props }) {
+  const activeStyles =
+    'border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium';
+
+  const inActiveStyles =
+    'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium';
+
+  return (
+    <>
+      {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+      <a href="#" className={`${activeStyles}`}>
+        {props.activeText}
+      </a>
+      <a href="#" className={`${inActiveStyles}`}>
+        {props.inActiveText}
+      </a>
+    </>
   );
 }
